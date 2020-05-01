11abe9da9b4c468febb59b6ed0fd3381773144e1
asifkottakal1 <asifkottakal1@gmail.com>
2020-05-01 12:04:37 +0530

    first commit

---
+++ src/AddItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class AddItem extends React.Component {
+    
+     
+  render(){
+  
+      return(
+        <div>
+        <h3>Add Product</h3>
+        <input placeholder="Name"/>
+        <hr/>
+        </div>
+      )
+      
+    }
+  
+}
+      
+ 
+export default AddItem;
